e6f59955dc841244da4ed1bc6267f44d586aebe0
sjaymoon15 <sjaymoon15@gmail.com>
2017-08-15 22:05:25 -0700

    add button and textfield. outlets and actions

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nameField: UITextField!
+  
+  @IBOutlet weak var lyricsView: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,11 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  @IBAction func reset(_ sender: Any) {
+  }
 
+  @IBAction func displayLyrics(_ sender: Any) {
+  }
 
 }
 
